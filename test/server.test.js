c2c048abe4fad6a0bec97be9da712e299d804cc6
ciscodiz <ciscoadiz@gmail.com>
2022-02-07 18:50:30 +0000

    configurando mocha

--- test/server.test.js
+++ test/server.test.js
@@ -1,10 +1,13 @@
 const supertest = require('supertest');
-const server = require('app');
+const server = require('../app');
 const chai = require('chai');
 
 chai.should();
 const api = supertest(server);
-
 describe('server run', () => {
-    api.get('/');
+    api.get('/').send().end((err, res) => {
+        console.log(res.status)
+        res.status.should.equal(200);
+        done();
+    });
 });
